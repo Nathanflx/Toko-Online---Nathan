16fc59e08e493eeaacd5c784ca5094747a98f4cd
Nathanflx <nathanaelfh78@gmail.com>
2024-02-29 08:52:47 +0700

    kukira kocak

--- Toko_Online.java
+++ Toko_Online.java
@@ -1,8 +1,19 @@
 package toko_online;
+import java.util.Scanner;
 
 public class Toko_Online {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+       
+        Laporan report = new Laporan();
+        Karyawan employee = new Karyawan();
+        Barang item = new Barang();
+        Transaksi transaction = new Transaksi();
+        Member member = new Member();
+       
+        report.laporan(item);
+        report.laporan(member);
+        report.laporan(transaction, item);
     }
     
 }
